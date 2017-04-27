b3ad813f6633f23c14f9800891d8e773cbc85fd6
Hamid Palangi <hamidp@ece.ubc.ca>
2017-04-26 17:20:42 -0700

    Added the results of official SQUAD test for TPR model to experiments.

--- basic/clustering.py
+++ basic/clustering.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
 from sklearn.metrics import silhouette_score
 from sklearn.neighbors import NearestNeighbors
 from pylab import *
